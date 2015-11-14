c180178e32bcb0d10c9e575ab4b65525194d2dd3
Евгений Дац <hintoz@gmail.com>
2015-11-14 11:56:58 +0300

    application should work faster

--- CoreImageDemoSeptOktLesson2Level3/FilterPhotoViewController.m
+++ CoreImageDemoSeptOktLesson2Level3/FilterPhotoViewController.m
@@ -10,12 +10,14 @@
 
 @interface FilterPhotoViewController ()
 
+@property (strong, nonatomic) CIImage *ciimage;
 @property (weak, nonatomic) IBOutlet UIImageView *originalImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *filteredImageView;
 @property (weak, nonatomic) IBOutlet UISlider *slider;
 @property (weak, nonatomic) IBOutlet UILabel *filterValue;
 @property (weak, nonatomic) IBOutlet UILabel *maxFilterValue;
 @property (weak, nonatomic) IBOutlet UILabel *minFilterValue;
+@property (strong, nonatomic) CIContext *context;
 
 @end
 
@@ -33,26 +35,40 @@
 #pragma mark - Setup
 
 -(void)setup {
+    
+    self.context = [CIContext contextWithOptions:nil];
+
+    
     UIImage *image = [UIImage imageNamed:@"HDtimelapse.net_City_1150_hirez"];
     self.originalImageView.image = image;
     self.image = image;
+    
+    self.ciimage = [CIImage imageWithCGImage:self.image.CGImage];
+    
+    self.filter = [CIFilter filterWithName:@"CISepiaTone"];
+    [self.filter setValue:self.ciimage forKey:kCIInputImageKey];
     
     [self filterImage:@0.8];
 }
 
 -(void)filterImage:(id)value {
-    CIImage *imageToFilter = [CIImage imageWithCGImage:self.image.CGImage];
     
-    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone"];
-    [filter setValue:imageToFilter forKey:kCIInputImageKey];
+    CIFilter *filter = self.filter;
+    
     
     NSParameterAssert(filter);
     
     [filter setValue:value forKey:@"inputIntensity"];
     
+    
     CIImage *result = [filter outputImage];
     
-    self.filteredImageView.image = [UIImage imageWithCIImage:result];
+    CGImageRef resultRef = [self.context createCGImage:result fromRect:[result extent]];
+    
+    
+    self.filteredImageView.image = [UIImage imageWithCGImage:resultRef];
+    
+    CGImageRelease(resultRef);
     
     
 }
