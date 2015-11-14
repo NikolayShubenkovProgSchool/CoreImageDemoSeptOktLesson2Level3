f8d1e70159691539213948f6f6b50bf9250e67f2
Evgeny Dats <hintoz@gmail.com>
2015-11-14 14:17:26 +0300

    image picker for face detection

---
+++ CoreImageDemoSeptOktLesson2Level3/FaceDetectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FaceDetectionViewController.h
+//  CoreImageDemoSeptOktLesson2Level3
+//
+//  Created by hintoz on 14.11.15.
+//  Copyright © 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceDetectionViewController : UIViewController
+
+@end
---
+++ CoreImageDemoSeptOktLesson2Level3/FaceDetectionViewController.m
@@ -0,0 +1,62 @@
+//
+//  FaceDetectionViewController.m
+//  CoreImageDemoSeptOktLesson2Level3
+//
+//  Created by hintoz on 14.11.15.
+//  Copyright © 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+#import "FaceDetectionViewController.h"
+#import <CZPhotoPickerController/CZPhotoPickerController.h>
+
+@interface FaceDetectionViewController ()
+
+@property (nonatomic, strong) UIImage *image;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic, strong) CZPhotoPickerController *photoPicker;
+
+
+@end
+
+@implementation FaceDetectionViewController
+
+-(void) setup {
+    
+    self.imageView.image = self.image;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UI Events
+
+
+- (IBAction)selectPhotoPressed:(id)sender {
+    
+    self.photoPicker = [[CZPhotoPickerController alloc] initWithCompletionBlock:^(UIImagePickerController *imagePickerController, NSDictionary *imageInfoDict) {
+        UIImage *image = [imageInfoDict valueForKey:UIImagePickerControllerEditedImage];
+        if (!image) {
+            image = [imageInfoDict valueForKey:UIImagePickerControllerOriginalImage];
+        }
+        
+        if (image) {
+            self.image = image;
+            [self setup];
+        }
+        
+        [imagePickerController dismissViewControllerAnimated:YES completion:nil];
+    }];
+    
+    self.photoPicker.allowsEditing = YES;
+    [self.photoPicker presentFromViewController:self];
+}
+
+
+@end
