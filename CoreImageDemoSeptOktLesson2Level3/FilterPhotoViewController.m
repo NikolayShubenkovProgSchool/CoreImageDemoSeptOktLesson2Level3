ebf7f5986e1c8b31f88a60fb337f4d19a0108dbd
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2015-11-14 13:01:25 +0300

    added cascade filter added gallery and camera support

--- CoreImageDemoSeptOktLesson2Level3/FilterPhotoViewController.m
+++ CoreImageDemoSeptOktLesson2Level3/FilterPhotoViewController.m
@@ -8,6 +8,10 @@
 
 #import "FilterPhotoViewController.h"
 
+#import <CZPhotoPickerController/CZPhotoPickerController.h>
+
+#import "CIFilter+SpExtention.h"
+
 @interface FilterPhotoViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *originalImageView;
@@ -19,6 +23,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *maxFilterValue;
 @property (weak, nonatomic) IBOutlet UILabel *minFilterValue;
 @property (strong, nonatomic) CIContext *context;
+@property (strong, nonatomic) CZPhotoPickerController *photoPicker;
 
 @end
 
@@ -28,7 +33,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.image = [UIImage imageNamed:@"HDtimelapse.net_City_1150_hirez"];
+
+    [CIFilter sp_allFilteres];
     [self setup];
 }
 
@@ -38,9 +45,7 @@
     //Внутри этого объекта будет происходить вся фильтрация и прочее
     self.context = [CIContext contextWithOptions:nil];
     
-    UIImage *image = [UIImage imageNamed:@"HDtimelapse.net_City_1150_hirez"];
-    self.originalImageView.image = image;
-    self.image = image;
+    self.originalImageView.image = self.image;
     self.ciimage = [CIImage imageWithCGImage:self.image.CGImage];
     
     
@@ -75,8 +80,63 @@
 #pragma mark - UI Events
 
 - (IBAction)selectPhotoPressed:(id)sender {
-    
+    self.photoPicker = [[CZPhotoPickerController alloc]initWithCompletionBlock:^(UIImagePickerController *imagePickerController, NSDictionary *imageInfoDict) {
+        UIImage *image = [imageInfoDict valueForKey:UIImagePickerControllerEditedImage];
+        if (!image){
+            image = [imageInfoDict valueForKey:UIImagePickerControllerOriginalImage];
+        }
+        
+        if (image){
+            self.image = image;
+            [self setup];
+        }
+        
+        [imagePickerController dismissViewControllerAnimated:YES completion:nil];
+    }];
+    self.photoPicker.allowsEditing = YES;
+    [self.photoPicker presentFromViewController:self];
 }
+
+- (IBAction)showComplexFilter:(id)sender {
+    //Sepia filter
+    CIFilter *sepiaFilter = [CIFilter filterWithName:@"CISepiaTone"];
+    [sepiaFilter setValue:self.ciimage forKey:kCIInputImageKey];
+    
+    [sepiaFilter setValue:@(self.slider.value) forKey:@"inputIntensity"];
+    
+    //random
+    CIFilter *randomFilter = [CIFilter filterWithName:@"CIRandomGenerator"];
+    
+    //bright filter
+    CIFilter *brightFilter = [CIFilter filterWithName:@"CIColorControls"];
+
+    [brightFilter setValue:@0.5 forKey:@"inputBrightness"];
+    [brightFilter setValue:@0 forKey:@"inputSaturation"];
+    [brightFilter setValue:randomFilter.outputImage forKey:kCIInputImageKey];
+    
+    //картинка яркости c шумом
+    CIImage *randomNoiseImage = [[brightFilter outputImage] imageByCroppingToRect:self.ciimage.extent];
+    
+    //объединим шумы и засепенную картинку
+    CIFilter *lightBlend = [CIFilter filterWithName:@"CIHardLightBlendMode"];
+    [lightBlend setValue:sepiaFilter.outputImage forKey:kCIInputImageKey];
+    [lightBlend setValue:randomNoiseImage forKey:kCIInputBackgroundImageKey];
+    
+    CIFilter *finalFilter = [CIFilter filterWithName:@"CIVignette"];
+    [finalFilter setValue:lightBlend.outputImage forKey:kCIInputImageKey];
+    [finalFilter setValue:@3 forKey:@"inputIntensity"];
+    [finalFilter setValue:@20 forKey:@"inputRadius"];
+    
+    CIImage *result = [finalFilter outputImage];
+    
+    //С помощью уже созданного контекста создадим картинку
+    CGImageRef resultRef = [self.context createCGImage:result fromRect:[result extent]];
+    
+    self.filteredImageView.image = [UIImage imageWithCGImage:resultRef];
+    
+    CGImageRelease(resultRef);
+}
+
 
 - (IBAction)filterValueChanged:(UISlider *)sender {
     [self filterImage:@( sender.value )];
